39708adee67069ba2ad48047935ebf25a5422c6a
Humoyin <baxranoofxumoyun@gmail.com>
2024-04-02 17:54:24 +0500

    yananimadir

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-document.addEventListener('keydown',(e)=>{
-    console.log('code',e.code)
-    console.log('key',e.key)
-    console.log('keycode',e.keyCode)
+// document.addEventListener('keydown',(e)=>{
+//     console.log('code',e.code)
+//     console.log('key',e.key)
+//     console.log('keycode',e.keyCode)
 
+// })
+const insert=document.getElementById('insert')
+
+window.addEventListener('keydown',(e)=>{
+    insert.innerHTML=`
+    <div class='key'>
+        ${e.code}
+        <small>event.code</small>
+    </div>
+    <div class='key'>
+        ${e.key}
+        <small>event.key</small>
+    </div>
+    <div class='key'>
+        ${e.keyCode}
+        <small>event.keyCode</small>
+    </div>
+
+    `
+
+
+
+    
 })
